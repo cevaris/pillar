0e674ab6f6eeb2bb1e2ca568a1f5f563a591cb21
Adam Cardenas <cevaris@gmail.com>
2014-10-19 09:49:25 -0600

    got simple json serialize deserializer

--- lib/pillar/worker.rb
+++ lib/pillar/worker.rb
@@ -1,11 +1,37 @@
 module Pillar
+
+
+  module Serialization
+
+    def to_json(*a)
+      hash = {}
+      instance_variables.collect {|x| hash[x.to_s.tr('@','')]= instance_variable_get(x) }
+      {
+        'json_class'   => self.class.name,
+        'data'         => hash
+      }.to_json(*a)
+    end
+
+    def self.from_json(o)
+      o = JSON.parse o if o.class == String
+
+      clazz = Module.const_get o['json_class']
+      # puts o.inspect
+      clazz.new(o['data'])
+    end
+
+  end
   
   class Worker
+    include Serialization
+
     attr_accessor :env, :args
 
-    def initialize(env={}, args=['/bin/bash'])
-      @env  = env
-      @args = args
+    def initialize(params=nil)
+      params ||= { env: {}, args: ['/bin/bash'] }
+      params.each do |k,v|
+        instance_variable_set("@#{k}", v) unless v.nil?
+      end
     end
 
     def self.configure
@@ -20,29 +46,6 @@ module Pillar
       raise 'Method Not Implemented Error'
     end
 
-    def as_json(options = { })
-        {
-            "json_class" => self.class.name,
-            "data" => [ env, args ]
-        }
-    end
-    def to_json(*a)
-        as_json.to_json(*a)
-    end
-    def self.json_create(o)
-        new o["data"]["elements"]
-    end
-
-    # def to_json(*a)
-    #   {
-    #     'json_class'   => self.class.name,
-    #     'data'         => [ env, args ]
-    #   }.to_json(*a)
-    # end
-
-    def self.json_create(o)
-      new(*o['data'])
-    end
   end
 
 end
