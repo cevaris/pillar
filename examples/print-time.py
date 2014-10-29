b5ec42a674ce507e4742cc72eddcd436e896e7e7
Adam Cardenas <cevaris@gmail.com>
2014-10-29 13:28:12 -0600

    updated python time

--- examples/print-time.py
+++ examples/print-time.py
@@ -4,22 +4,29 @@ import os
 import atexit
 import logging
 import time
+import sys
+import signal
 
 #### Python Pillar client
 # Creates pid file
-PID_PATH = '/tmp/pillar'
+PID_PATH  = '/tmp/pillar'
+WORKER_ID = os.environ.get('WORKER_ID')
 pid = str(os.getpid())
-pidfile = "%s/pillar.%s.pid" % (PID_PATH, pid)
+pidfile = "%s/pillar.%s.pid" % (PID_PATH, WORKER_ID)
 if os.path.isfile(pidfile):
     print "%s already exists, exiting" % pidfile
     sys.exit()
 else:
     file(pidfile, 'w').write(pid)
-def on_exit():
-    os.unlink(pidfile)
 
+def cleanup(*args):
+    # print "Removing file %s" % pidfile
+    if os.path.exists(pidfile):
+        os.remove(pidfile)
+    sys.exit()
 
-atexit.register(on_exit)
+# atexit.register(cleanup)
+signal.signal(signal.SIGTERM, cleanup)
 ####
 
 
