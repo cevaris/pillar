b4ac8b7f7f7bb97e24120044ed7bd902d6ff3dd8
Adam Cardenas <cevaris@gmail.com>
2014-10-30 10:33:46 -0600

    stupid rake install

--- lib/pillar/worker.rb
+++ lib/pillar/worker.rb
@@ -4,10 +4,10 @@ module Pillar
   class Worker
     include Pillar::Serialization
 
-    attr_accessor :worker_id, :env, :args
+    attr_accessor :worker_id, :args
 
     def initialize(params=nil)
-      params ||= { env: {}, args: [] }
+      params['args']      ||= []
       params['worker_id'] ||= SecureRandom.hex
       params.each do |k,v|
         instance_variable_set("@#{k}", v) unless v.nil?
@@ -16,12 +16,12 @@ module Pillar
 
     def spawn
       # @args << '&'
-      @env['WORKER_ID'] = @worker_id
       raise "No file/script defined" if @args.count < 1
-      raise "File/Script does not exist: #{@args[0]}" if not File.exist?(@args[0])
+      # raise "File/Script does not exist: #{@args[0]}" if not File.exist?(@args[0])
+      raise "Valid File/Script not found: #{@args}" if @args.select {|f| File.exist?(f)}.empty?
       puts "Execting #{@args}"
       # (pid = fork) ? (Process.detach(pid); puts "Executed child PID#{pid}") : exec(@args.join(' '))
-      pid = Process.spawn(@env, @args.join(' '))
+      pid = Process.spawn({ 'WORKER_ID' => @worker_id }, @args.join(' '))
       Process.detach(pid) if pid
       # (pid = fork) ? (Process.detach(pid); puts "Executed child PID#{pid}") : exec(@args.join(' '))
     end
