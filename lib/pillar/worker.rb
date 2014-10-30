a8c0599a666758eda131c503ff90e8c7c6c8a582
Adam Cardenas <cevaris@gmail.com>
2014-10-30 13:26:06 -0600

    added daemon flag

--- lib/pillar/worker.rb
+++ lib/pillar/worker.rb
@@ -4,12 +4,14 @@ module Pillar
   class Worker
     include Pillar::Serialization
 
-    attr_accessor :worker_id, :args
+    attr_accessor :worker_id, :args, :daemon
+    alias_method :is_daemon?, :daemon
 
     def initialize(params=nil)
-      params['args']      ||= []
+      params['args']      ||= ""
       params['worker_id'] ||= SecureRandom.hex
       params['host']      ||= HOSTNAME
+      params['daemon']    = true if params['daemon'].nil?
       params.each do |k,v|
         instance_variable_set("@#{k}", v) unless v.nil?
       end
@@ -17,12 +19,12 @@ module Pillar
 
     def spawn
       # @args << '&'
-      raise "No file/script defined" if @args.count < 1
+      raise "No file/script defined" if @args.empty?
       # raise "File/Script does not exist: #{@args[0]}" if not File.exist?(@args[0])
-      raise "Valid File/Script not found: #{@args}" if @args.select {|f| File.exist?(f)}.empty?
-      puts "Execting #{@args}"
+      raise "Valid File/Script not found in: #{@args}" if @args.split(' ').select {|f| File.exist?(f)}.empty?
+      puts "Executing #{@args}"
       # (pid = fork) ? (Process.detach(pid); puts "Executed child PID#{pid}") : exec(@args.join(' '))
-      pid = Process.spawn({ 'WORKER_ID' => @worker_id }, @args.join(' '))
+      pid = Process.spawn({ 'WORKER_ID' => @worker_id }, @args)
       Process.detach(pid) if pid
       # (pid = fork) ? (Process.detach(pid); puts "Executed child PID#{pid}") : exec(@args.join(' '))
     end
