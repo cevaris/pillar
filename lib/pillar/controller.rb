990076092cb2724318c675fde1fdee30cf26def1
Adam Cardenas <cevaris@gmail.com>
2014-10-30 13:36:19 -0600

    added port to hostname

--- lib/pillar/controller.rb
+++ lib/pillar/controller.rb
@@ -4,28 +4,25 @@ module Pillar
     def initialize
       puts Pillar.configuration.inspect
     end
-    def self.rackup
-      SinatraController.run!
-    end
   end
   
   class SinatraController < Sinatra::Base
+      set :port, 4444
+
       get '/v1/' do
         {status: 'ok'}.to_json
       end
 
       get '/v1/host' do
-        {host: Pillar::HOSTNAME}.to_json
+        {host: "#{Pillar::HOSTNAME}:#{settings.port}"}.to_json
       end
 
       post '/v1/workers/' do
         request.body.rewind
         payload = JSON.parse request.body.read
 
-        # puts "Path:#{payload['args']}"
         worker = Pillar::Worker.new 'args' => payload['args'], 
-                                    'daemon' => payload['args'], 
-                                    'host' => Pillar::HOSTNAME
+                                    'daemon' => payload['daemon']
         puts "Worker: #{worker.inspect}"
 
         worker.to_json
