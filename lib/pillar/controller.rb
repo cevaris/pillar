6f32f8f7335e75987f8115efe19a6fef079ab171
Adam Cardenas <cevaris@gmail.com>
2014-11-04 19:39:33 -0700

    got working kill endpoint

--- lib/pillar/controller.rb
+++ lib/pillar/controller.rb
@@ -9,12 +9,32 @@ module Pillar
   class SinatraController < Sinatra::Base
     set :port, 4444
 
+    OK = {status: 'ok'}.to_json
+
     get '/v1/' do
-      {status: 'ok'}.to_json
+      OK
     end
 
     get '/v1/host' do
       {host: "#{Pillar::HOSTNAME}:#{settings.port}"}.to_json
+    end
+
+    get '/v1/workers/' do
+      {workers: Pillar::EtcdWorker.all.map {|w| w.from_json}}.to_json
+    end
+
+    delete '/v1/workers/:worker_id' do
+      worker = worker_by_path("/workers/#{params[:worker_id]}")
+      puts "Deleting worker #{worker.worker_id}"
+      pidfile = "#{PID_DIR}/pillar.#{worker.worker_id}.pid"
+      deleted_worker = Pillar::EtcdWorker.delete(worker)
+      puts "Deleted from Etcd #{deleted_worker.inspect}"
+      if File.exists?(pidfile)
+        puts "Found file #{pidfile}"
+        Process.kill(15, File.read(pidfile).to_i)
+        puts "Killed worker"
+      end
+      OK
     end
 
     post '/v1/workers/' do
--- lib/pillar/persistence.rb
+++ lib/pillar/persistence.rb
@@ -1,18 +1,31 @@
 module Pillar
 
   class Persistance
-    def get_client; raise NotImplementedError end 
-    def save(obj);  raise NotImplementedError end
+    def get_client;  raise NotImplementedError end 
+    def all;         raise NotImplementedError end 
+    def save(obj);   raise NotImplementedError end
+    def delete(obj); raise NotImplementedError end
   end
   
   class EtcdWorker < Persistance
     def self.get_client
+      # Etcd.client( host: '127.0.0.1', port: 4001 )
       Etcd.client( host: Pillar.configuration.etcd_hosts, port: 4001 )
+    end
+
+    def self.all
+      client = self.get_client
+      client.get('/workers', {recursive: true, dir: true, sorted: true}).children.collect(&:value) || []
     end
 
     def self.save(obj)
       client = self.get_client
       client.set("/workers/#{obj.worker_id}", value: obj.to_json)
+    end
+
+    def self.delete(obj)
+      client = self.get_client
+      client.delete("/workers/#{obj.worker_id}")
     end
   end
 
